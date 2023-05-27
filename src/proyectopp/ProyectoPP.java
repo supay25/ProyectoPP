d3fe6f626dc9b184bd9bd8d4380e942d74b44d2d
supay25 <134749575+supay25@users.noreply.github.com>
2023-05-26 18:59:00 -0600

    Trabajar aqui

--- src/proyectopp/ProyectoPP.java
+++ src/proyectopp/ProyectoPP.java
@@ -15,6 +15,8 @@ public class ProyectoPP {
      */
     public static void main(String[] args) {
         System.out.println("Proyecto");
+        
+        System.out.println("Actualizacion");
     }
     
 }
