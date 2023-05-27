c5d64a47fbb9863e91df250d9b29e62df68933e6
supay25 <134749575+supay25@users.noreply.github.com>
2023-05-26 18:37:08 -0600

    Este es el codigo fuente

---
+++ src/proyectopp/ProyectoPP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectopp;
+
+/**
+ *
+ * @author luisd
+ */
+public class ProyectoPP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Proyecto");
+    }
+    
+}
