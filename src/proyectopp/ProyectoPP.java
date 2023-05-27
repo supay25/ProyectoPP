33f8c3d8c7e44c00e91168552b7fa6722899ed4d
supay25 <134749575+supay25@users.noreply.github.com>
2023-05-26 19:17:12 -0600

    Update ProyectoPP.java
    Queriendo actualizar

--- src/proyectopp/ProyectoPP.java
+++ src/proyectopp/ProyectoPP.java
@@ -16,7 +16,7 @@ public class ProyectoPP {
     public static void main(String[] args) {
         System.out.println("Proyecto");
         
-        System.out.println("Actualizacion 2");
+        System.out.println("Veneco");
     }
     
 }
